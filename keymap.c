52bcf30d1826dbbc510d4220d9d19516a8b2067a
Hannah Morrison <hannah@landhmorrison.scot>
2023-09-21 21:15:32 +0100

    Add ../ macro

--- keymap.c
+++ keymap.c
@@ -11,6 +11,7 @@ enum my_layers {
 enum my_keycodes {
     M_ESCQ = SAFE_RANGE,
     M_ESCW,
+    M_DDS,
     M_ALTT,
     M_NDESK,
     M_PDESK,
@@ -45,16 +46,23 @@ static bool kc_del_registered = false;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE_LAYER] = LAYOUT_split_3x5_2(
-    KC_Q,           LCTL_T(KC_W),  LALT_T(KC_F),  LGUI_T(KC_P),  KC_B,     KC_J,           LGUI_T(KC_L),  LALT_T(KC_U),  LCTL_T(KC_Y),  KC_BSPC,
-    KC_A,           KC_R,          KC_S,          KC_T,          KC_G,     KC_M,           KC_N,          KC_E,          KC_I,          KC_O,
-    KC_Z,           KC_X,          KC_C,          KC_D,          KC_V,     KC_K,           KC_H,          KC_COMM,       KC_DOT,        OSL(SCUT_LAYER),
-    OSM(MOD_LSFT),  LT(NAV_LAYER, KC_SPC),       LT(NUM_LAYER, KC_ENT),  OSL(SYM_LAYER)
+    KC_Q,           LCTL_T(KC_W),           LALT_T(KC_F),           LGUI_T(KC_P),   KC_B,  KC_J,  LGUI_T(KC_L),  LALT_T(KC_U),  LCTL_T(KC_Y),  KC_BSPC,
+    KC_A,           KC_R,                   KC_S,                   KC_T,           KC_G,  KC_M,  KC_N,          KC_E,          KC_I,          KC_O,
+    KC_Z,           KC_X,                   KC_C,                   KC_D,           KC_V,  KC_K,  KC_H,          KC_COMM,       KC_DOT,        OSL(SCUT_LAYER),
+    OSM(MOD_LSFT),  LT(NAV_LAYER, KC_SPC),  LT(NUM_LAYER, KC_ENT),  OSL(SYM_LAYER)
+  ),
+
+  [SYM_LAYER] = LAYOUT_split_3x5_2(
+    KC_EXLM,  LSFT(KC_2),     LSFT(KC_3),  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,        KC_ASTR,        KC_UNDS,  KC_PLUS,
+    KC_TAB,   LSFT(KC_NUBS),  KC_LBRC,     KC_LCBR,  KC_LPRN,  KC_COLN,  LSFT(KC_QUOT),  LSFT(KC_BSLS),  KC_MINS,  KC_EQL,
+    CW_TOGG,  KC_NUBS,        KC_RBRC,     KC_RCBR,  KC_RPRN,  KC_SCLN,  KC_QUOT,        KC_BSLS,        KC_GRV,   KC_QUES,
+    KC_TRNS,  KC_TRNS,        KC_TRNS,     KC_TRNS
   ),
 
   [NAV_LAYER] = LAYOUT_split_3x5_2(
-    KC_NO,    KC_PSCR,  M_ISCB,   M_ISWIN,  KC_INS,   M_XTAB,  M_PDESK,  LCTL(KC_TAB),  M_ALTT,   M_NDESK,
-    KC_NO,    KC_MNXT,  KC_MPLY,  KC_VOLU,  KC_BRIU,  KC_WH_U,   KC_LEFT,  KC_DOWN,       KC_UP,    KC_RGHT,
-    KC_NO,    KC_MPRV,  KC_MUTE,  KC_VOLD,  KC_BRID,  KC_WH_D,   KC_HOME,  KC_PGDN,       KC_PGUP,  KC_END,
+    KC_NO,    KC_PSCR,  M_ISCB,   M_ISWIN,  KC_INS,   M_XTAB,   M_PDESK,  LCTL(KC_TAB),  M_ALTT,   M_NDESK,
+    KC_NO,    KC_MNXT,  KC_MPLY,  KC_VOLU,  KC_BRIU,  KC_WH_U,  KC_LEFT,  KC_DOWN,       KC_UP,    KC_RGHT,
+    KC_NO,    KC_MPRV,  KC_MUTE,  KC_VOLD,  KC_BRID,  KC_WH_D,  KC_HOME,  KC_PGDN,       KC_PGUP,  KC_END,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
@@ -68,15 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SCUT_LAYER] = LAYOUT_split_3x5_2(
     M_ESCQ,   M_ESCW,      M_NTRM,      M_1PASS,           M_LIKE,      KC_NO,  HYPR(KC_M),  HYPR(KC_A),  HYPR(KC_K),  KC_NO,
     KC_ESC,   M_APP1,      M_APP2,      M_APP3,            M_APP4,      KC_NO,  KC_NO,       M_EMOJI,     KC_NO,       KC_NO,
-    KC_CAPS,  LCTL(KC_X),  LCTL(KC_C),  LSFT(LCTL(KC_C)),  LCTL(KC_V),  KC_NO,  KC_NO,       KC_NO,       KC_NO,       KC_SLSH,
+    KC_CAPS,  LCTL(KC_X),  LCTL(KC_C),  LSFT(LCTL(KC_C)),  LCTL(KC_V),  KC_NO,  KC_NO,       KC_NO,       M_DDS,       KC_SLSH,
     KC_TRNS,  KC_TRNS,     KC_TRNS,     KC_TRNS
-  ),
-
-  [SYM_LAYER] = LAYOUT_split_3x5_2(
-    KC_EXLM,         LSFT(KC_2),     LSFT(KC_3),  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,        KC_ASTR,        KC_UNDS,  KC_PLUS,
-    KC_TAB,          LSFT(KC_NUBS),  KC_LBRC,     KC_LCBR,  KC_LPRN,  KC_COLN,  LSFT(KC_QUOT),  LSFT(KC_BSLS),  KC_MINS,  KC_EQL,
-    CW_TOGG,         KC_NUBS,        KC_RBRC,     KC_RCBR,  KC_RPRN,  KC_SCLN,  KC_QUOT,        KC_BSLS,        KC_GRV,   KC_QUES,
-    CW_TOGG,  KC_TAB,         KC_TRNS,     KC_TRNS
   ),
 
 };
@@ -114,15 +115,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case M_ESCQ:
       if (record->event.pressed) {
         SEND_STRING(SS_TAP(X_ESC)":q!"SS_TAP(X_ENT));
-      } else {
-        layer_move(BASE_LAYER);
       }
       break;
     case M_ESCW:
       if (record->event.pressed) {
         SEND_STRING(SS_TAP(X_ESC)":wq"SS_TAP(X_ENT));
-      } else {
-        layer_move(BASE_LAYER);
+      }
+      break;
+    case M_DDS:
+      if (record->event.pressed) {
+        SEND_STRING("../");
       }
       break;
     case M_ALTT:
